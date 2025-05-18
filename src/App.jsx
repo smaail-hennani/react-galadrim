ca3299a523e701975e0b02aefa9e7663a52a0a10
smaail-hennani <gylgamesh34x@gmail.com>
2025-05-18 18:24:03 +0200

    feat: intégration du composant Counter typé avec valeur initiale passée en prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Counter from './components/Counter'
 
@@ -11,7 +9,7 @@ function App() {
     <>
       <div>
         <h1>Bienvenue dans l'app React Galadrim</h1>
-        <Counter />
+        <Counter initial={10} />
       </div>
     </>
   )
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,13 +1,21 @@
 import React, {useState} from 'react';
 
-function Counter() {
-    const [count, setCount] = useState(0);
+type CounterProps = {
+    initial: number;
+}
+
+function Counter( { initial }: CounterProps) {
+    const [count, setCount] = useState<number>(initial);
+
+    const reset = () => setCount(initial);
 
     return (
-        <div style={{ padding: '1rem', border: '1px solid gray', borderRadius: '8px' }}>
+        <div style={{ padding: '1rem', border: '1px solid gray', borderRadius: '8px', marginTop: '1rem' }}>
             <h2>Compteur</h2>
             <p>Valeur actuelle : {count}</p>
+            <p>Valeur x2 : {count * 2}</p>
             <button onClick={() => setCount(count + 1)}>Incrémenter</button>
+            <button onClick={reset} style={{ marginLeft: '1rem' }}>Reset</button>
         </div>
     );
 }
