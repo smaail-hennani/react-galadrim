4369d6e07580a3fbd599d0fb8745bda7574b567e
smaail-hennani <gylgamesh34x@gmail.com>
2025-05-18 19:18:14 +0200

    feat: ajout d’un champ input pour définir dynamiquement la valeur du compteur

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -6,8 +6,16 @@ type CounterProps = {
 
 function Counter( { initial }: CounterProps) {
     const [count, setCount] = useState<number>(initial);
+    const [inputValue, setInputValue] = useState<string>("");
 
     const reset = () => setCount(initial);
+
+    const updateInitial = () => {
+        const parsed = parseInt(inputValue);
+        if (!isNaN(parsed)) {
+            setCount(parsed);
+        }
+    };
 
     return (
         <div style={{ padding: '1rem', border: '1px solid gray', borderRadius: '8px', marginTop: '1rem' }}>
@@ -16,6 +24,16 @@ function Counter( { initial }: CounterProps) {
             <p>Valeur x2 : {count * 2}</p>
             <button onClick={() => setCount(count + 1)}>Incrémenter</button>
             <button onClick={reset} style={{ marginLeft: '1rem' }}>Reset</button>
+
+            <div style={{ marginTop: '1rem'}}>
+                <input 
+                    type="number"
+                    placeholder="Entrer une valeur initiale"
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                />
+                <button onClick={updateInitial} style={{ marginLeft: '1rem'}}> Définir </button>
+            </div>
         </div>
     );
 }
